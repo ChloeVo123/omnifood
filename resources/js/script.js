162c1fc8e61c7e81bb41b975855edd82ad2588a8
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-02-19 16:20:15 +1100

    navigation responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -73,4 +73,20 @@ $(document).ready(function () {
     }, {
         offset: '50%'
     })
+
+
+    /* Mobile Nav */
+    $('.js--nav-icon').click(function() {
+        const nav = $('.js--main-nav');
+        const icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    })
 })
